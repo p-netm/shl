ba0b5a185d9f61929f9ff23b0b72815441877e17
= <pmuriuki98@gmail.com>
2017-09-20 12:03:53 +0300

    [feature] add root application details

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,45 @@
-__author__ = 'Sudo Pnet'
+from flask import Flask
+import os
+from flask_mail import Mail
+from flask_moment import Moment
+from shl.config import config
+from shl.app.main import shl
+from flask_login import LoginManager
+from shl.app.auth import auth
+from flask_script import Manager, Shell
+from shl.app.models import User, ShoppingList
+
+login_manager = LoginManager()
+mail = Mail()
+moment = Moment()
+
+
+def create_app(configuration_name='default'):
+    """ factory function: used to delay the creation of the application instance
+    so that configuration details can be selected"""
+    app = Flask(__name__)
+    # make sure configuration is valid
+    configuration_list = ['development', 'testing', 'production', 'default']
+    if configuration_name not in configuration_list:
+        raise ValueError('Unknown configuration argument')
+    app.config.from_object(config[configuration_name])
+    config[configuration_name].init_app(app)
+
+    mail.init_app(app)
+    moment.init_app(app)
+    login_manager.init_app(app)
+
+    # register the shl blueprint: comes alive with the application instance
+    app.register_blueprint(shl)
+    app.register_blueprint(auth)
+
+    return app
+
+manager = Manager()
+
+@manager.shell
+def make_shell_context():
+    return dict(app=create_app(os.getenv('COFIGURATION')), User=User, ShoppingList=ShoppingList)
+
+from shl.app.main import views, errors, forms
+from shl.app.auth import views, forms
