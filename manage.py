74a4e8c323b23378ecb2ebbb68326b0059fbf500
= <pmuriuki98@gmail.com>
2017-09-20 16:05:58 +0300

    [fix] add shell configuration details

--- manage.py
+++ manage.py
@@ -1,1 +1,16 @@
-__author__ = 'Sudo Pnet'
+from flask_script import Manager, Shell
+from shl.app.models import User, ShoppingList
+from shl.app import create_app
+import os
+
+
+app = create_app(os.getenv('COFIGURATION'))
+manager = Manager(app=app)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, User=User, ShoppingList=ShoppingList)
+
+
+if __name__ == '__main__':
+    manager.run()
