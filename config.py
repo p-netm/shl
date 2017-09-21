c39ef90d9a9197295a9c9b4abae300af2af9ec75
= <pmuriuki98@gmail.com>
2017-09-21 10:27:55 +0300

    [Fix] removed unimplemented environment variables declarations

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ import os
 
 class Configuration:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adau fagkfa821b 32bdc^!$@sad'
-    FLASKY_MAIL_SUBJECT_PREFIX = "SHOPPING LIST.COM"
-    FLASKY_MAIL_SENDER = ''
 
     @staticmethod
     def init_app(app):
@@ -14,14 +12,11 @@ class Configuration:
 
 class DevelopmentConfiguration(Configuration):
     DEBUG = True
-    MAIL_SERVER = ''
-    MAIL_PORT = ''
-    MAIL_USE_TLS = ''
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
+
 
 class TestingConfiguration(Configuration):
     TESTING = True
+    DEBUG = True
 
 
 class ProductionConfiguration(Configuration):
