d492873b01f47eff146e57228108bb9b168c9761
= <pmuriuki98@gmail.com>
2017-09-21 16:06:30 +0300

    [FIX] add deploy options

--- manage.py
+++ manage.py
@@ -8,6 +8,16 @@ app = create_app(os.environ.get('CONFIGURATION'))
 manager = Manager(app=app)
 
 
+def make_shell_context():
+    return dict(app=app, User=User, Shoppinglist=ShoppingList)
+
+manager.add_command('shell', Shell(make_context=make_shell_context()))
+
+
+@manager.command
+def start():
+    """deploy"""
+
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
