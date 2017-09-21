8e59040697f49b33c641d6598d9d3df7cbf862e9
= <pmuriuki98@gmail.com>
2017-09-21 11:34:16 +0300

    [feature] add routes and logic to auth blueprint

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,16 +1,42 @@
 from flask_wtf import Form
-from wtforms import PasswordField, SubmitField, StringField
-from wtforms.validators import DataRequired, Email
+from wtforms import PasswordField, SubmitField, StringField, ValidationError
+from wtforms.validators import DataRequired, Email, length, Regexp, EqualTo
+from ..models import User, Gears
+
+gear = Gears()
 
 
 class RegistrationForm(Form):
-    """ Used to model the registation form"""
-    name = StringField('', validators=[DataRequired()])
-    user_name = StringField('', validators=[DataRequired()])
-    email = StringField('', validators=[DataRequired(), Email()])
-    passcode = PasswordField('')
-    C_passcode = PasswordField('')
+    """ Used to model the registration form"""
+    name = StringField('', validators=[DataRequired(), length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_ ]*$',
+                                                                             0, 'Name can only contain numbers, '
+                                                                                'space or underscores')])
+    user_name = StringField('', validators=[DataRequired(),
+                                            length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$',
+                                                                  0, 'Name can only contain numbers, '
+                                                                     'dots or underscores')])
+    email = StringField('', validators=[DataRequired(), Email(), length(1, 64)])
+    passcode = PasswordField('', validators=[DataRequired()])
+    C_passcode = PasswordField('', validators=[DataRequired(), EqualTo(passcode, 'Password do not match')])
     sign_up = SubmitField('Sign up')
+
+    def validate_email(self, field):
+        email = field.data
+        """custom validation that checks if a user details are already being used"""
+        # if email in user.emails then raise a validation error
+        try:
+            gear.validate_email(email)
+        except Exception:
+            raise ValidationError
+
+    def validate_username(self, field):
+        """ validates that the username is unique to the system"""
+        user_name = field.data
+        # if user_name in user.usernames then raise a validationerror
+        try:
+            gear.validate_user_name(user_name)
+        except Exception:
+            raise ValidationError
 
 
 class LoginForm(Form):
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,18 @@
 from flask import render_template, session, redirect, url_for, request, flash
 from . import auth
 from .forms import RegistrationForm, LoginForm
-from ..models import User
-from flask_login import login_user, login_required
+from ..models import User, Gears
+from flask_login import login_user, login_required, logout_user
+
+
+gear = Gears()
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        # get user by email
+        # get user by email -> tricky
         email = login_form.email.data
         user = User.get_user_by_email(email)
         passcode = login_form.passcode.data
@@ -19,3 +22,25 @@ def login():
         else:
             flash('invalid email or password')
     return render_template('login.html', form=login_form)
+
+
+@auth.route('/log_out')
+@login_required
+def logout():
+    logout_user()  # removes and resets a user session
+    flash('GOODBYE')
+    return redirect(url_for('shl.index'))
+
+
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        email = reg_form.email.data
+        user_name = reg_form.user_name.data
+        passcode = reg_form.passcode.data
+        name = reg_form.name.data
+        gear.add_user(email=email, password=passcode, name=name, user_name=user_name)
+        flash('You have been successfully registered')
+        return redirect('auth.login')
+    return render_template('register.html', form=reg_form)
