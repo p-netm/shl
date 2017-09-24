15839021386dbad7d25cf74450ef6196763b0af6
= <pmuriuki98@gmail.com>
2017-09-24 22:36:25 +0300

    [fix]

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,15 +3,15 @@ from . import shl
 
 
 @shl.app_errorhandler(404)
-def page_not_found():
-    return render_template('404.html'), 404
+def page_not_found(e):
+    return render_template('info/404.html'), 404
 
 
 @shl.app_errorhandler(500)
-def internal_issues():
-    return render_template('500.html'), 500
+def internal_issues(e):
+    return render_template('info/500.html'), 500
 
 
 @shl.app_errorhandler(405)
-def method_not_allowed():
-    return render_template('405.html', 405)
+def method_not_allowed(e):
+    return render_template('info/405.html'), 405
