2a79216bcb1c43580105ebef98e8f74598b76f6c
= <pmuriuki98@gmail.com>
2017-09-27 14:37:55 +0300

    [update] add cacheable field

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,10 +15,11 @@ def login():
         # get user by email -> tricky
         email = login_form.email.data
         user = gear.get_user_by_email(email)
+        remember = login_form.rem.data
         passcode = login_form.passcode.data
         if user and user.check_password(passcode):
             logging_in_user = user
-            login_user(logging_in_user)
+            login_user(logging_in_user, remember)
             return redirect(url_for('shl.index') or request.args.get('next'))
         else:
             flash('invalid email or password', 'danger')
